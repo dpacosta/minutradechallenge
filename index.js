edc631e70aa8641a32e6aa6880dd64efdc4c0f51
Daniel <danieldepaulaa@gmail.com>
2016-05-13 01:47:54 -0300

    refactoring continuation

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var express = require('express');
-var app =  express();
-var bodyParser = require('body-parser');
 var router = express.Router();
-
+var app = express();
+var bodyParser = require('body-parser');
 var clients = require('./lib/clients');
-clients.routes(router);
 
+clients.routes(router);
+app.use('/', router);
 app.use(express.static(__dirname + "/app"));
 app.use(bodyParser.json());
 app.listen(3000);
